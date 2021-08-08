398dc84679a95c592107e7f04ad2200f4294157f
CheeHung <hung_1008@live.com>
2021-08-08 23:34:30 +0800

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,28 @@
 import { Component, VERSION } from '@angular/core';
 import { of, from } from 'rxjs';
+import { map, take, tap } from 'rxjs/operators';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent  {
+export class AppComponent {
   name = 'Angular ' + VERSION.major;
 
   ngOnInit() {
-    of(2, 4, 6, 8).subscribe(console.log);
+    of(2, 4, 6, 8)
+      .pipe(
+        // tap(item => console.log(item)),
+        map(item => item * 2),
+        take(2)
+      )
+      .subscribe(console.log);
 
     from([2, 4, 6, 8]).subscribe(
       item => console.log(`item... ${item}`),
       err => console.error(`error occurred ${err}`),
       () => console.log(`complete`)
-
-    )
+    );
   }
 }
